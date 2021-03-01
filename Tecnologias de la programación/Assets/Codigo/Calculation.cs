7e9399745b9c4cdcca15dce524d7d08a4fafda81
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2021-02-28 19:32:25 -0600

    tarea static terminado

--- Tecnologias de la programación/Assets/Codigo/Calculation.cs
+++ Tecnologias de la programación/Assets/Codigo/Calculation.cs
@@ -4,24 +4,20 @@ using UnityEngine;
 
 public static class Calculation
 {
-    //used for sum
-    static int number1 = 5;
-    static int number2 = 4;
+    static int number1 = 10;
+    static int number2 = 3;
 
-    //used for substraction
-    static int number3 = 8;
-    static int number4 = 10;
-
-    static void Sum(int result1)
+    public static int Sum (int numberOne, int numberTwo)
     {
-       result1 = number1 + number2;
-       Debug.Log(result1);
+        int result;
+        result = numberOne + numberTwo;
+        return result;
     }
 
-    static void Substraction(int result2)
+    public static int Substraction(int numberOne, int numberTwo)
     {
-       result2 = number3 - number4;
-       Debug.Log(result2);
+        int result;
+        result = numberOne - numberTwo;
+        return result;
     }
-
 }
